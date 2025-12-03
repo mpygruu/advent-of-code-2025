6969318b9491271b0ffad19fdb0ca46a73142689
mpygruu <barkwa67@gmail.com>
2025-12-03 12:11:49 +0100

    Add day 3 solution

---
+++ day03/part1.py
@@ -0,0 +1,32 @@
+import os 
+
+FILE_NAME = os.path.join(os.getcwd(), "day03", "data.txt")
+
+file = open(FILE_NAME, "r")
+
+def find_first_max_val(line: str) -> (int, int):
+    max_val, max_val_idx = 0, 0
+    for i, v in enumerate(line):
+        v = int(v)
+        if v > max_val:
+            max_val = v
+            max_val_idx = i
+    
+    return max_val, max_val_idx
+
+joltages = []
+for line in file:
+    line = line.strip()
+
+    max_val, max_val_idx = find_first_max_val(line)
+    
+    if max_val_idx == len(line)-1:
+        tens_digit = max(line[:-1])
+        joltages.append(int(f"{tens_digit}{max_val}"))
+    else:
+        ones_digit = max(line[max_val_idx+1:])
+        joltages.append(int(f"{max_val}{ones_digit}"))
+    
+    print(line, joltages[-1])
+
+print(sum(joltages))
---
+++ day03/part2.py
@@ -0,0 +1,30 @@
+import os 
+
+FILE_NAME = os.path.join(os.getcwd(), "day03", "data.txt")
+
+file = open(FILE_NAME, "r")
+
+def find_first_max_val(line: str, start_idx:int, stop_idx: int) -> (int, int):
+    max_val, max_val_idx = 0, 0
+    for i in range(start_idx, stop_idx+1):
+        v = int(line[i])
+        if v > max_val:
+            max_val = v
+            max_val_idx = i
+    
+    return max_val, max_val_idx
+
+joltages = []
+for line in file:
+    line = line.strip()
+    current_joltage = ""
+    last_idx = -1
+
+    for i in range(12, 0, -1):
+        max_val, max_val_idx = find_first_max_val(line, last_idx+1, len(line)-i)
+        current_joltage += str(max_val)
+        last_idx = max_val_idx
+    
+    joltages.append(int(current_joltage))
+
+print(sum(joltages))
