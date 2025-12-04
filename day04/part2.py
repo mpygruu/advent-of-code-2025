10031c86bdda993c086818781dcd9b75fbe4fb13
mpygruu <barkwa67@gmail.com>
2025-12-04 13:02:06 +0100

    Add day 4 solution

---
+++ day04/part2.py
@@ -0,0 +1,52 @@
+import os
+
+FILE_NAME = os.path.join(os.getcwd(), "day04", "data.txt")
+
+
+def prepare_grid():
+    first_line = file.readline().strip()
+    grid = [list('.' * (len(first_line) + 2)), list('.' + first_line + '.')]
+
+    for line in file:
+        grid.append(list('.' + line.strip() + '.'))
+
+    grid.append(list('.' * (len(first_line) + 2)))
+
+    return grid
+
+
+def number_of_adjacent_paper_rolls(grid, i ,j):
+    count = 0
+    for k in range(i-1, i+2):
+        for n in range(j-1, j+2):
+            if grid[k][n] == '@' and not (k == i and n == j):
+                count += 1
+
+    return count
+
+
+file = open(FILE_NAME, 'r')
+
+grid = prepare_grid()
+
+accessible_paper_rolls_count = 0;
+paper_roll_removed = True
+
+while paper_roll_removed:
+    paper_roll_removed = False
+    coords_to_remove = []
+
+    for i in range(1, len(grid)-1):
+        for j in range(1, len(grid[0])-1):
+            if grid[i][j] == '@' and number_of_adjacent_paper_rolls(grid, i, j) < 4:
+                accessible_paper_rolls_count += 1
+                coords_to_remove.append((i,j))
+                paper_roll_removed = True
+    
+    for coords in coords_to_remove:
+        grid[coords[0]][coords[1]] = '.'
+
+for row in grid:
+    print(row)
+
+print('\n', accessible_paper_rolls_count)
